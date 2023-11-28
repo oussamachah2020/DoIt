7ba60317ac38eb1c9530367d00957d57e3d46020
oussamachah2020 <otaksam213@gmail.com>
2023-11-28 21:30:26 +0100

    wip

--- src/components/AvatarUploader.tsx
+++ src/components/AvatarUploader.tsx
@@ -1,10 +1,29 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 type Props = {};
 
 const AvatarUploader = (props: Props) => {
+  const pickImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    // No permissions request is necessary for launching the image library
+    if (status) {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        console.log(result.assets[0].uri);
+      }
+    }
+  };
+
   return (
     <TouchableOpacity
       style={{
@@ -20,6 +39,7 @@ const AvatarUploader = (props: Props) => {
         flex: 1,
         elevation: 2,
       }}
+      onPress={pickImage}
     >
       <Ionicons name="camera-sharp" size={20} style={{}} color="#2F89FC" />
     </TouchableOpacity>
